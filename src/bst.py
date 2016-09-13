08ca88da33e72c8d74e7623ce8719d570f422fe0
Mike <mikekh@uw.edu>
2016-09-13 15:36:40 -0700

    did contain and tests. Started on depth.

--- src/bst.py
+++ src/bst.py
@@ -10,6 +10,7 @@ class Node(object):
         self.parent = None
         self.lchild = None
         self.rchild = None
+        self._depth = 0
 
 
 class BinarySearchTree(object):
@@ -30,23 +31,57 @@ class BinarySearchTree(object):
         if self.root is None:
             self.root = new_node
             self._len += 1
+            self.root._depth = 1
         else:
             current_node = self.root
             check = True
+            counter = 1
             while check is True:
                 if new_node.data < current_node.data:
                     if current_node.lchild:
                         current_node = current_node.lchild
+                        counter += 1
                     else:
                         current_node.lchild = new_node
                         new_node.parent = current_node
                         self._len += 1
+                        if current_node.rchild is None:
+                            self.root._depth += 1
                         check = False
                 elif new_node.data > current_node.data:
                     if current_node.rchild:
                         current_node = current_node.rchild
+                        counter += 1
                     else:
                         current_node.rchild = new_node
                         new_node.parent = current_node
                         self._len += 1
+                        if current_node.lchild is None:
+                            self.root._depth += 1
                         check = False
+
+    def contains(self, data):
+        """Check if data is present in bst."""
+        check = False
+        current_node = self.root
+        while check is False:
+            if data == current_node.data:
+                check = True
+            elif data < current_node.data:
+                if current_node.lchild:
+                    current_node = current_node.lchild
+                else:
+                    break
+            elif data > current_node.data:
+                if current_node.rchild:
+                    current_node = current_node.rchild
+                else:
+                    break
+        return check
+
+    def depth(self):
+        """Return depth of bst."""
+        try:
+            return self.root._depth
+        except AttributeError:
+            return 0
--- src/test_bst.py
+++ src/test_bst.py
@@ -1,6 +1,18 @@
 """Module of Testing for Binary Search Tree."""
 
-import pytest
+
+def test_empty_bst():
+    """Test an empty bst class."""
+    from bst import BinarySearchTree as BsT
+    test_bst = BsT()
+    assert test_bst.size() == 0
+
+
+def test_empty_root():
+    """Test root is None on empty bst."""
+    from bst import BinarySearchTree as BsT
+    test_bst = BsT()
+    assert test_bst.root is None
 
 
 def test_insert_on_empty_size():
@@ -105,3 +117,124 @@ def test_insert_data_18(bst_three):
     bst_three.insert(20)
     bst_three.insert(18)
     assert bst_three.root.rchild.rchild.lchild.data == 18
+
+
+def test_contains_on_one(bst_one):
+    """Test bst has the item."""
+    assert bst_one.contains(10) is True
+
+
+def test_contains_on_one_fail(bst_one):
+    """Test to see a failure."""
+    assert bst_one.contains(79) is False
+
+
+def test_contains_on_three(bst_three):
+    """Test bst with 3 nodes contains true."""
+    assert bst_three.contains(7)
+
+
+def test_contains_on_three_right(bst_three):
+    """Test bst with 3 nodes right true."""
+    assert bst_three.contains(15)
+
+
+def test_contains_on_three_false(bst_three):
+    """Test bst with three nodes returns False."""
+    assert bst_three.contains(79) is False
+
+
+def test_contains_with_more_nodes(bst_three):
+    """Test contains with lots of nodes."""
+    bst_three.insert(9)
+    bst_three.insert(20)
+    bst_three.insert(18)
+    bst_three.insert(347)
+    bst_three.insert(5)
+    bst_three.insert(1)
+    assert bst_three.contains(18)
+
+
+def test_contains_with_more_nodes2(bst_three):
+    """Test contains with lots of nodes."""
+    bst_three.insert(9)
+    bst_three.insert(20)
+    bst_three.insert(18)
+    bst_three.insert(347)
+    bst_three.insert(5)
+    bst_three.insert(1)
+    assert bst_three.contains(1)
+
+
+def test_contains_with_more_nodes3(bst_three):
+    """Test contains with lots of nodes."""
+    bst_three.insert(9)
+    bst_three.insert(20)
+    bst_three.insert(18)
+    bst_three.insert(347)
+    bst_three.insert(5)
+    bst_three.insert(1)
+    assert bst_three.contains(347)
+
+
+def test_contains_with_more_nodes4(bst_three):
+    """Test contains with lots of nodes."""
+    bst_three.insert(9)
+    bst_three.insert(20)
+    bst_three.insert(18)
+    bst_three.insert(347)
+    bst_three.insert(5)
+    bst_three.insert(1)
+    assert bst_three.contains(79) is False
+
+
+def test_contains_with_more_nodes5(bst_three):
+    """Test contains with lots of nodes."""
+    bst_three.insert(9)
+    bst_three.insert(20)
+    bst_three.insert(18)
+    bst_three.insert(347)
+    bst_three.insert(5)
+    bst_three.insert(1)
+    assert bst_three.contains(0) is False
+
+
+def test_depth_on_empty():
+    """Test depth is 0."""
+    from bst import BinarySearchTree as BsT
+    test_bst = BsT()
+    assert test_bst.depth() == 0
+
+
+def test_depth_on_one(bst_one):
+    """Test depth is one on bst of 1."""
+    assert bst_one.depth() == 1
+
+
+def test_depth_on_three(bst_three):
+    """Test depth witj three nodes."""
+    assert bst_three.depth() == 2
+
+
+def test_depth_on_4(bst_three):
+    """Test depth of 4 nodes."""
+    bst_three.insert(20)
+    assert bst_three.depth() == 3
+
+
+def test_depth_of_5_nodes(bst_three):
+    """Test depth of 5 nodes."""
+    bst_three.insert(20)
+    bst_three.insert(18)
+    assert bst_three.depth() == 4
+
+
+def test_depth_of_lefts_and_rights(bst_three):
+    """Check depth on bigger bst."""
+    bst_three.insert(9)
+    bst_three.insert(20)
+    bst_three.insert(18)
+    bst_three.insert(347)
+    bst_three.insert(5)
+    bst_three.insert(1)
+    assert bst_three.depth() == 4
